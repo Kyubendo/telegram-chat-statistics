ad065fe8320d43d4c491111d941713bd13a56168
Igor <kotigo111@gmail.com>
2022-01-20 13:21:36 +0200

    add time map statistic

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, {useState} from 'react';
 import './App.css';
+import {FileInput} from "./fileInput";
+import {Chart} from "./Chart";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+const App = () => {
+    const [data, setData] = useState<object>();
+    const [fileChosen, setFileChosen] = useState(false);
+    return <div className="App">
+        <FileInput setData={setData} setFileChosen={setFileChosen}/>
+        {
+            fileChosen ?
+                data ? <Chart rawData={data}/> : 'Loading...'
+                : ''
+        }
     </div>
-  );
+
 }
 
 export default App;
